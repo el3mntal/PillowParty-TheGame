717e47a8526f3b4e7ab7ac0c8115651a445b8b12
el3mntal <jhohan64@hotmail.com>
2025-05-26 11:54:47 -0500

    Mejora Desactivar UI y Activar Puntaje Final

--- Assets/UI/PantallaPuntajeFinal/DesactivarCanvasPuntajeFinal.cs
+++ Assets/UI/PantallaPuntajeFinal/DesactivarCanvasPuntajeFinal.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class DesactivarCanvasPuntajeFinal : MonoBehaviour
 {
+    [SerializeField] private GameObject canvasMostrar; // Primer Canvas que se muestra cuando el temporizador llega a 0
+    [SerializeField] private GameObject canvasOcultar; // Segundo Canvas que se oculta cuando el temporizador llega a 0
+    [SerializeField] private GameObject temporizadorObj; // GameObject con el script ControlTemporizador
+
+    private ControlTemporizador temporizador;
+
+    private void Start()
+    {
+        if (temporizadorObj != null)
+        {
+            temporizador = temporizadorObj.GetComponent<ControlTemporizador>();
+        }
+    }
+
+    private void Update()
+    {
+        if (temporizador != null)
+        {
+            bool tiempoFinalizado = temporizador.RestanteTiempo <= 0;
+
+            // Mostrar el primer Canvas cuando el tiempo es 0
+            canvasMostrar.SetActive(tiempoFinalizado);
+
+            // Ocultar el segundo Canvas cuando el tiempo es 0
+            canvasOcultar.SetActive(!tiempoFinalizado);
+        }
+    }
+
+
+    /*
     [SerializeField] private GameObject canvas; // Referencia al Canvas
     [SerializeField] private GameObject temporizadorObj; // Referencia al GameObject con el script
 
@@ -24,5 +54,5 @@ public class DesactivarCanvasPuntajeFinal : MonoBehaviour
             // Mostrar Canvas cuando el tiempo es 0, ocultarlo si es mayor a 0
             canvas.SetActive(temporizador.RestanteTiempo <= 0);
         }
-    }
+    }*/
 }
